29133c23c052616a66ddf3f4e4698f6366196a6a
Jordan Dundas <dundas.jordan21@gmail.com>
2022-05-03 20:37:18 -0500

    Change Doc

--- Changes.R
+++ Changes.R
@@ -3,3 +3,6 @@
 # Added more lines to remove side-scrolling, added space b/w new pieces of code
 # Added code to save graph for yolk height vs cue type
 # Changed point sizes to be equal across all graphs (Point size 3, error bar size 1
+# Added photo of embryo in markdown doc
+# Edited readxl command to no longer include the file search function.
+# Should now automatically grab the xl file without searching.
